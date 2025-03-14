a88c36ffbd8425f4e4123bd27cff014a2193a2c7
Aayush Rai <aayushrai0911@gmail.com>
2025-03-14 12:13:02 +0530

    github pages previews setup

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
+		},
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		prerender: {
 			entries: ['*']
 		},
