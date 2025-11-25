6db48aa7dd376d1de4d34969ebfdd1d7fff84f90
Aayush Rai <aayushrai0911@gmail.com>
2025-11-25 20:46:26 +0530

    -

--- src/lib/services/map.ts
+++ src/lib/services/map.ts
@@ -445,7 +445,7 @@ function loadBusModel(modelId: string, coords: [number, number], bearing: number
 		obj: BUS_GLB_URL,
 		type: 'gltf',
 		scale: initialScale,
-		units: 'scene',
+		units: 'meters',
 		rotation: {
 			x: 0,
 			y: 0, // + bearing,
